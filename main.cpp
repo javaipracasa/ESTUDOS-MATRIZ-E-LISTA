822a32218eb095f4404ad864a5596e77b45fbb49
Javaipacasa <viniciusdeaquinogomez@gmail.com>
2026-09-23 00:40:07 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2, soma;
+
+    cout<<"INSIRA DOIS NUMEROS"<<endl;
+    //primeiro numero
+    cout<<"INSIRA O PRIMEIRO NUMERO: ";
+    cin>>num1;
+
+    cout<<"INSIRA O SEGUNDO NUMERO: ";
+    cin>>num2;
+
+    soma = num1 + num2;
+
+    cout<<"O RESULTADO DESSA SOMA E: "<<soma;
+
+
+
+
+
+
+}
